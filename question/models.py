c5e3f36edeb27274f14aaef9553f2652842c0b65
suasue <devsu7477@gmail.com>
2021-05-16 01:26:25 +0900

    Create: create models

--- question/models.py
+++ question/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Question(models.Model):
+    title      = models.CharField(max_length=255)
+    content    = models.TextField(null=True)
+    author     = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+    
+    class Meta:
+        db_table = 'questions'
+
+
+class Comment(models.Model):
+    content    = models.TextField()
+    author     = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    question   = models.ForeignKey('Question', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'comments'
+
+
+class QuestionLike(models.Model):
+    user     = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'question_likes'
--- user/models.py
+++ user/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        user = self.model(
+            email = self.normalize_email(email),
+            name  = name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None):
+        user = self.create_user(
+            email    = email,
+			name     = name,
+            password = password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+	email      = models.EmailField(max_length=255, unique=True)
+	name       = models.CharField(max_length=255)
+	is_active  = models.BooleanField(default=True)
+	is_staff   = models.BooleanField(default=False)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	deleted_at = models.DateTimeField(null=True)
+
+	class Meta:
+		db_table = 'users'
+
+	objects = UserManager()
+
+	USERNAME_FIELD  = 'email'
+	REQUIRED_FIELDS = ['name']
+	
